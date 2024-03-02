e43647ebdbf595c816a2a6e1403e99fe3485333d
Sulphate7433 <161150336+Sulphate7433@users.noreply.github.com>
2024-03-02 00:00:30 +0000

    45 Minutes in Tutorial

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
-from flask import Flask
-
-app = Flask(__name__)                     # Create a flask application
-
-@app.route('/')                           # Creating a route to the homepage 'wepage.com/'
-
-def hello_world():                        # Creating a function
-    return '<p>Hello, World!</p>'
+from flask import Flask, render_template
 
 
-if __name__ == '__main__':                # Runnning the app if the file is run directly
-    app.run(host='0.0.0.0',debug=True)
+# Create a flask application
+app = Flask(__name__)  
+# Creating a route to the homepage 'wepage.com/'
+@app.route('/')  
+def hello_world(): 
+  return render_template('home.html')
+
+
+
+# Runnning the app if the file is run directly
+if __name__ == '__main__':  
+  app.run(host='0.0.0.0', debug=True)
