15ea6e5dd45a44f37cfa57b297aafdd7e44d07f8
Sulphate7433 <Sulphate7433>
2024-03-03 18:24:13 -0500

    Added database.py

---
+++ database.py
@@ -0,0 +1,22 @@
+import urllib
+from sqlalchemy import create_engine,text
+import pandas as pd
+
+server   = 'budget-app.database.windows.net'
+database = 'TestDB'
+username = 'Augusto'
+password = '!QAZ2wsx'   
+driver   = '{ODBC Driver 18 for SQL Server}'
+
+conn = f"""Driver={driver};Server=tcp:{server},1433;Database={database};
+Uid={username};Pwd={password};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"""
+
+params = urllib.parse.quote_plus(conn)
+conn_str = 'mssql+pyodbc:///?autocommit=true&odbc_connect={}'.format(params)
+engine = create_engine(conn_str, echo=True)
+
+query = 'SELECT @@version'
+
+with engine.connect() as connection:
+    df = pd.read_sql_query(sql=text(query), con=connection)
+print(df.to_markdown())
