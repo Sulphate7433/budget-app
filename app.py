98db6d611d12269f4d2cd01ddf1b7127f566216a
Sulphate7433 <161150336+Sulphate7433@users.noreply.github.com>
2024-03-03 01:22:27 +0000

    Templated app

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 from flask import Flask, render_template
 
-
-
-
-
-
 # Create a flask application
-app = Flask(__name__)  
-
-
+app = Flask(__name__)
 
 
 # Creating a route to the homepage 'wepage.com/'
-@app.route('/')  
-def hello_world(): 
+@app.route('/')
+def hello_world():
   return render_template('home.html')
 
 
-
 # Runnning the app if the file is run directly
-if __name__ == '__main__':  
+if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
