5c2b5d3e5ef7cae1bbc5ba39bb8b60e50627c561
Sulphate7433 <161150336+Sulphate7433@users.noreply.github.com>
2024-03-03 03:04:40 +0000

    Added /login/ and /budgetapp/

--- app.py
+++ app.py
@@ -6,9 +6,16 @@ app = Flask(__name__)
 
 # Creating a route to the homepage 'wepage.com/'
 @app.route('/')
-def hello_world():
+def home():
   return render_template('home.html')
-
+  
+@app.route('/budgetapp/')
+def budgetapp():
+    return render_template('budgetapp.html')
+  
+@app.route('/login/')
+def login():
+    return render_template('login.html')
 
 # Runnning the app if the file is run directly
 if __name__ == '__main__':
